0aecfc3ac976b975c2d298510271f67f5635e058
Martin Kenney <martin@localhost.localdomain>
2018-11-15 10:08:32 +0000

    Basic spring boot project

---
+++ analysis-platform/src/main/java/com/kenesys/analysisplatform/AnalysisPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.kenesys.analysisplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisPlatformApplication.class, args);
+	}
+}
