bbf279774919f6adbb15f23430897b994184468b
Martin Kenney <martin@localhost.localdomain>
2018-11-19 15:12:21 +0000

    Add some swagger metadata

--- analysis-platform/src/main/java/com/kenesys/analysisplatform/config/SwaggerConfig.java
+++ analysis-platform/src/main/java/com/kenesys/analysisplatform/config/SwaggerConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,14 +17,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.kenesys.analysisplatform"))
                 //.paths(PathSelectors.regex("/templates.*"))
-                .build();
+                .build()
+                .apiInfo(metaData());
+    }
 
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Azure Analysis Platform REST API")
+                .description("\"Spring Boot REST API for Azure Analysis Platform\"")
+                .version("0.1")
+                .build();
     }
 
     @Override
