4afd50c5de6e1e2d85265d0fed79cfbdd298b08a
Martin Kenney <martin.kenney@kenesys.co.uk>
2018-12-04 14:50:58 +0000

    Add resource request providing template via muli-part form data

--- analysis-platform/src/main/java/com/amplify/ap/services/resources/ResourceService.java
+++ analysis-platform/src/main/java/com/amplify/ap/services/resources/ResourceService.java
@@ -4,7 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
@@ -32,5 +37,19 @@ public class ResourceService {
         sb.append(template.getAbsolutePath());
         sb.append("}");
         LOGGER.info(sb.toString());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body
+                = new LinkedMultiValueMap<>();
+        body.add("resource-group", resourceGroup);
+        body.add("instance-id", instanceId);
+        body.add("file", template);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity
+                = new HttpEntity<>(body, headers);
+
+        restTemplate.postForEntity(resourceRequestUrl, requestEntity, String.class);
     }
 }
