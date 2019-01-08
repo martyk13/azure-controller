aa115c98fadca38712ac33ce8a2bc7d6b86dedee
Andy <andy@localhost.localdomain>
2019-01-08 08:50:01 +0000

    Modified the default region as the value has to match that in the Region class

--- analysis-platform-controller/src/main/java/com/amplify/apc/services/azure/AzureServiceCloud.java
+++ analysis-platform-controller/src/main/java/com/amplify/apc/services/azure/AzureServiceCloud.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import com.amplify.apc.domain.ResourceType;
 import com.fasterxml.jackson.databind.JsonNode;
