49228b7143e0af9a79df96d1488ad591e26350bf
Andy <andy@Andys-MBP.home>
2020-03-31 09:34:23 +0100

    Some slight changes to the deploy-to-azure script to reflect recent azure changes

--- scripts/deploy-to-azure.sh
+++ scripts/deploy-to-azure.sh
@@ -12,51 +12,69 @@ AZ_APC_DEPLOY_NAME=${AZ_APC_DEPLOY_NAME:-${AZ_APC_NAME}}${AZ_NAME_QUALIFIER}
 AP_VERSION=${AP_VERSION:-0.0.1-SNAPSHOT}
 APC_VERSION=${AP_VERSION}
 AP_TAG=${AP_TAG:-v0.1}
+RUN=
 debug=false
+doCreate=false
+doDelete=false
 
 ### Functions ###
+
+USAGE() {
+  echo ""
+  echo "`basename $0` [--debug] [--create | --delete] [--noex]"
+  echo ""
+  echo "    --create | -c : Create resources"
+  echo "    --delete | -d : Delete resources"
+  echo "    --debug  | -x : Emit debug output"
+  echo "    --noex   | -n : Do not execute - just show commands that would be executed"
+  echo ""
+  exit 1
+}
+
 INFO() {
-  echo ">>>> $1"
+  echo "`date +%Y-%m-%d:%H:%M:%S` >>>> $1"
 }
 
 DEBUG() {
   if [ "${debug}" == "true" ]; then
-    echo ">>>> $1"
+    echo "`date +%Y-%m-%d:%H:%M:%S` >>>> $1"
   fi
 }
 
 createResource() {
-    INFO "Creating resource group ${AZ_RG_NAME}"
-    az group create --name ${AZ_RG_NAME} --location ${AZ_REGION}
+    INFO "Creating resources in Resource Group ${AZ_RG_NAME}"
+    ${RUN} az group create --name ${AZ_RG_NAME} --location ${AZ_REGION}
 
     # Create the DB
     DEBUG "Creating cosmosdb ${AZ_MONGO_DB} (mongo mode)"
-    az cosmosdb create --name ${AZ_MONGO_DB} --resource-group ${AZ_RG_NAME} --kind MongoDB
-    DB_KEY=`az cosmosdb list-keys --name ${AZ_MONGO_DB} --resource-group ${AZ_RG_NAME} --query "primaryMasterKey"`
+    ${RUN} az cosmosdb create --name ${AZ_MONGO_DB} --resource-group ${AZ_RG_NAME} --kind MongoDB
+    DB_KEY=`${RUN} az cosmosdb keys list --name ${AZ_MONGO_DB} --resource-group ${AZ_RG_NAME} --query "primaryMasterKey"`
     MONGODB_URI="mongodb://${AZ_MONGO_DB}:${DB_KEY//\"}@${AZ_MONGO_DB}.documents.azure.com:10255/ap?ssl=true&sslverifycertificate=false"
 
     # Create private docker repo in resource group and push the image
     DEBUG "Creating private docker repo ${AZ_DOCKER_REPO}"
-    az acr create --resource-group ${AZ_RG_NAME} --name ${AZ_DOCKER_REPO} --sku Basic --admin-enabled true
-    az acr login --name ${AZ_DOCKER_REPO}
-    NAMESPACE=`az acr show --name ${AZ_DOCKER_REPO} --query loginServer`
+    ${RUN} az acr create --resource-group ${AZ_RG_NAME} --name ${AZ_DOCKER_REPO} --sku Basic --admin-enabled true
+    ${RUN} az acr login --name ${AZ_DOCKER_REPO}
+    
+    NAMESPACE=`${RUN} az acr show --name ${AZ_DOCKER_REPO} --query loginServer`
 
     # Tag and push the AP
     DEBUG "Tagging and pushing the Analysis Platform"
-    docker tag amplify/${AZ_AP_NAME}:${AP_VERSION} ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
-    docker push ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} docker tag amplify/${AZ_AP_NAME}:${AP_VERSION} ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} docker push ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
     # Tag and push the APC
     DEBUG "Tagging and pushing the Analysis Platform Controller"
-    docker tag amplify/${AZ_APC_NAME}:${APC_VERSION} ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
-    docker push ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} docker tag amplify/${AZ_APC_NAME}:${APC_VERSION} ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} docker push ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
 
-    PASSWORD=`az acr credential show --name ${AZ_DOCKER_REPO} --query "passwords[0].value"`
+    PASSWORD=`${RUN} az acr credential show --name ${AZ_DOCKER_REPO} --query "passwords[0].value"`
+
     # Load the auth file
     source $AZURE_AUTH_LOCATION
 
     # Start the AP container instance
     DEBUG "Starting the Analysis Platform Controller container instance ${AZ_APC_DEPLOY_NAME}"
-    az container create \
+    ${RUN} az container create \
         --resource-group ${AZ_RG_NAME} \
         --name ${AZ_APC_DEPLOY_NAME} \
         --image ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG} \
@@ -71,11 +89,12 @@ createResource() {
             AZURE_LOGIN_DOMAIN=${tenant} \
             AZURE_LOGIN_SECRET=${key}  \
         --ports 8080
-    APC_IP=`az container show --name ${AZ_APC_DEPLOY_NAME} --resource-group ${AZ_RG_NAME} | jq -r '.ipAddress.ip'`
+
+    APC_IP=`${RUN} az container show --name ${AZ_APC_DEPLOY_NAME} --resource-group ${AZ_RG_NAME} | jq -r '.ipAddress.ip'`
 
     # Start the AP container instance
     DEBUG "Starting the Analysis Platform container instance ${AZ_AP_DEPLOY_NAME}"
-    az container create \
+    ${RUN} az container create \
         --resource-group ${AZ_RG_NAME} \
         --name ${AZ_AP_DEPLOY_NAME} \
         --image ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG} \
@@ -93,17 +112,16 @@ createResource() {
 
 deleteResource() {
     INFO "Deleting resources"
-    NAMESPACE=`az acr show --name ${AZ_DOCKER_REPO} --query loginServer`
-    docker rmi ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
-    docker rmi ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
-    az group delete --name ${AZ_RG_NAME} -y
+    NAMESPACE=`${RUN} az acr show --name ${AZ_DOCKER_REPO} --query loginServer`
+    ${RUN} docker rmi ${NAMESPACE//\"}/amplify/${AZ_AP_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} docker rmi ${NAMESPACE//\"}/amplify/${AZ_APC_DEPLOY_NAME}:${AP_TAG}
+    ${RUN} az group delete --name ${AZ_RG_NAME} -y
 }
 
 ### MAIN ###
-USAGE="USAGE: [--debug] --create(-c), --delete(-d)"
 
 if [ $# -eq 0 ]; then
-    echo $USAGE
+    USAGE
 fi
 
 # Switch between modes using flags
@@ -111,21 +129,36 @@ while [ ! $# -eq 0 ]
 do
 	case "$1" in
 		--create | -c)
-			createResource
-			exit
+			doCreate=true
 			;;
 		--delete | -d)
-			deleteResource
-			exit
+			doDelete=true
 			;;
+                --noex | -n)
+                        RUN="echo"
+                        ;;
                 --debug | -x)
                         debug=true
                         ;;
 		*)
 		    echo $USAGE
-		    exit
 		    ;;
 	esac
 	shift
 done
+
+if [ "${doCreate}" == "true" ] && [ "${doDelete}" == "true" ]; then
+  echo "Cannot specify to CREATE (--create) and DELETE (--delete) in same command"
+  exit 1
+fi
+
+if [ "${doCreate}" == "true" ]; then
+  createResource
+  exit 0
+fi
+
+if [ "${doDelete}" == "true" ]; then
+  deleteResource
+  exit 0
+fi
 
